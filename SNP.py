89401b3e1cc71707a6c1b99162d3d30a65597f23
AmirHoseinSafari <54955625+AmirHoseinSafari@users.noreply.github.com>
2020-04-25 18:17:32 -0700

    Update SNP.py

--- SNP.py
+++ SNP.py
@@ -6,10 +6,13 @@ FILE_NAME = "."
 
 
 ###############################################
-# in this code by using the phenotypes id in the "FILE_NAME".txt file
-# and its .fastq files we will make its SNP usinf bwa and samtools
-# if the code fail in make SNP of special phenotype, the id will be
-# write in the "missIdForSNP.txt"
+# In this code by using the phenotypes id in the "FILE_NAME".txt file and its .fastq files we will 
+# make its SNP using bwa and samtools and GATK and get the common SNP of both files and store in ("Final_" + id + ".vcf")
+#
+# If the code fail in make SNP of special phenotype, the id will be write in the "missIdForSNP.txt"
+#
+# By calling preprocessing() function you will get the ( "Final_" + id + "_table.csv" ) file which 
+# contatin "pos, ref, alt" for each SNP
 ###############################################
 
 def cloneDirectories():
